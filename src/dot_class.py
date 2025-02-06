0330cdff05fbef7a0327a8d76b35343ca0759221
H-Manuel <hm2799@mci4me.at>
2025-02-06 11:02:19 +0100

    erstellen der Punkt Llasse und ihrer Tochterklassen

---
+++ src/dot_class.py
@@ -0,0 +1,25 @@
+from typing import Tuple
+
+class dot:
+    '''
+    A class that represents a point in a 2D plane. '''
+    def __init__(self, x, y):
+        self._x = x
+        self._y = y
+
+    def set_coordinates(self, x, y):
+        self._x = x
+        self._y = y
+
+    def get_coordinates(self)-> Tuple[float, float]:
+        return (self._x, self._y)
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+    
+    def __repr__(self):
+        return self.__str__()
+
+
+if __name__ == "__main__":
+    d = dot(1, 2)
+    print(d)
