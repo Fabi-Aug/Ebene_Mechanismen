fb69c72577157d3bfba2f1532219bfa671887db3
H-Manuel <hm2799@mci4me.at>
2025-02-06 11:18:30 +0100

    added typehints in dot classes

--- src/dot_class.py
+++ src/dot_class.py
@@ -1,21 +1,24 @@
 from typing import Tuple
 
+
 class dot:
-    '''
-    A class that represents a point in a 2D plane. '''
+    """
+    A class that represents a point in a 2D plane."""
+
     def __init__(self, x, y):
         self._x = x
         self._y = y
 
-    def set_coordinates(self, x, y):
+    def set_coordinates(self, x: float, y: float):
         self._x = x
         self._y = y
 
-    def get_coordinates(self)-> Tuple[float, float]:
+    def get_coordinates(self) -> Tuple[float, float]:
         return (self._x, self._y)
+
     def __str__(self):
         return f"({self.x}, {self.y})"
-    
+
     def __repr__(self):
         return self.__str__()
 
--- src/swivel_class.py
+++ src/swivel_class.py
@@ -6,31 +6,34 @@ from singleton import singleton
 
 @singleton
 class swivel(dot):
-    '''
-    A class that represents a swivel point in a 2D plane. It inherits from the dot class. Phi is the current angle in rad of the swivel point.'''
-    def __init__(self, x, y, r, phi):
+    """
+    A class that represents a swivel point in a 2D plane. It inherits from the dot class. Phi is the current angle in rad of the swivel point.
+    """
+
+    def __init__(self, x: float, y: float, r: float, phi: float):
         super().__init__(x, y)
         self._r = r
         self._phi = phi
 
-    def set_phi(self, phi):
+    def set_phi(self, phi: float):
         self._phi = phi
 
-    def get_phi(self)->float:
+    def get_phi(self) -> float:
         return self._phi
-    
+
     def get_circlepoint(self) -> Tuple[float, float]:
         return (self._x + self._r * cos(self._phi), self._y + self._r * sin(self._phi))
-    
+
     def set_coordinates(self, x, y):
         pass
-    
+
     def __str__(self):
         return f"({self._x}, {self._y}, {self._r}, {self._phi})"
 
     def __repr__(self):
         return self.__str__()
-    
+
+
 if __name__ == "__main__":
     s = swivel(1, 2, 3, 4)
     print(s)
