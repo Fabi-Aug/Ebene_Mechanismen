920319ebd8054648d053e0682ad316b9ab071faa
H-Manuel <hm2799@mci4me.at>
2025-02-06 11:43:29 +0100

    added instances to dot classes

--- src/connectinglinks_class.py
+++ src/connectinglinks_class.py
@@ -1,15 +1,25 @@
 from dot_class import dot
 
 class connectionlinks:
+    '''Class to represent a connection between two dots'''
+
+    _instances = []
+
     def __init__(self, dot1: dot, dot2: dot):
         self.dot1 = dot1
         self.dot2 = dot2
+        self.__class__._instances.append(self)
 
     def calc_length(self) -> float:
         x1,y1 = self.dot1.get_coordinates()
         x2,y2 = self.dot2.get_coordinates()
         length = ((x2-x1)**2 + (y2-y1)**2)**0.5	
         return length
+    
+    @classmethod
+    def get_instances(cls):
+        """Gibt alle erstellten Instanzen der Klasse zurück."""
+        return cls._instances
     
     def __str__(self):
         return f"Link between {self.dot1} and {self.dot2} with length {self.calc_length()}"
--- src/dot_class.py
+++ src/dot_class.py
@@ -5,9 +5,12 @@ class dot:
     """
     A class that represents a point in a 2D plane."""
 
+    _instances = []
+
     def __init__(self, x, y):
         self._x = x
         self._y = y
+        self.__class__._instances.append(self)
 
     def set_coordinates(self, x: float, y: float):
         self._x = x
@@ -15,6 +18,11 @@ class dot:
 
     def get_coordinates(self) -> Tuple[float, float]:
         return (self._x, self._y)
+    
+    @classmethod
+    def get_instances(cls):
+        """Gibt alle erstellten Instanzen der Klasse zurück."""
+        return cls._instances
     
     def __str__(self):
         return f"({self._x}, {self._y})"
